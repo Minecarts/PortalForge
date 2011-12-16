e95cb41b4910e224eef0e773b0c8d0067ad92589
Stephen <stephen@jazer.com>
2011-12-15 20:46:50 -0500

    Removal of 1.0.19 files

--- src/com/minecarts/portalforge/portal/PortalActivation.java
+++
@@ -1,6 +0,0 @@
-package com.minecarts.portalforge.portal;
-
-public enum PortalActivation {
-    INSTANT,
-    DELAYED
-}
--- src/com/minecarts/portalforge/portal/PortalType.java
+++
@@ -1,9 +0,0 @@
-package com.minecarts.portalforge.portal;
-
-public enum PortalType {
-    NETHER,
-    HOME,
-    GENERIC,
-    SKYLAND,
-    END
-}
